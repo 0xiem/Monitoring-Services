5caf558cf0c319e6ef74412d413d5aeaf0b46174
0xiem <issam.moussaoui@outlook.com>
2025-10-09 12:45:46 +0200

    Mod : Solucion de error que no va por [espacio]

--- MonitoringServices.sh
+++ MonitoringServices.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 CheckServiceWeb=$(systemctl is-active apache2)
 CheckServiceBD=$(systemctl is-active mysql)
-if [$CheckServiceWeb -eq "active"];then
+
+if [ "$CheckServiceWeb" = "active" ];then
     echo "[Activo] apache2"
-elif [$CheckServiceBD -eq "active"];then
+elif [ "$CheckServiceBD" = "active" ];then
     echo "[Activo] mysql"
 else
     echo "[Error] Alguna cosa no va"
