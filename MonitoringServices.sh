6597f0a4307bb95a1d7c0d81581f842bb3f55467
0xiem <issam.moussaoui@outlook.com>
2025-10-09 12:35:08 +0200

    MOD: dectectar Los servicios si van o no

--- MonitoringServices.sh
+++ MonitoringServices.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+CheckServiceWeb=$(systemctl is-active apache2)
+CheckServiceBD=$(systemctl is-active mysql)
+if [$CheckServiceWeb -eq "active"];then
+    echo "[Activo] apache2"
+elif [$CheckServiceBD -eq "active"];then
+    echo "[Activo] mysql"
+else
+    echo "[Error] Alguna cosa no va"
+fi
