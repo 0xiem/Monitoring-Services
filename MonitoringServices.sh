a38ef0cad120710b0abd6641a710a154edc623a8
0xiem <issam.moussaoui@outlook.com>
2025-10-12 19:55:16 +0200

    He mejorado el script

--- MonitoringServices.sh
+++ MonitoringServices.sh
@@ -2,10 +2,10 @@
 CheckServiceWeb=$(systemctl is-active apache2)
 CheckServiceBD=$(systemctl is-active mysql)
 
-if [ $CheckServiceWeb = 'inactive' && $CheckServiceBD = 'inactive' ];then
-    echo  '['date'] [Alerta] Els serveis apache2 i mysql no estan actius al servidor ' hostname
+if [[ $CheckServiceWeb = "inactive" && $CheckServiceBD = "inactive" ]] ;then
+    echo  '['$(date)'] [Alerta] Els serveis apache2 i mysql no estan actius al servidor ' $(hostname)
 elif [ $CheckServiceWeb = 'inactive'  ];then
-    echo  '['date'] [Alerta] El servei apache2  no esta actiu al servidor ' hostname
+    echo  '['$(date)'] [Alerta] El servei apache2  no esta actiu al servidor ' $(hostname)
 elif [ $CheckServiceBD = 'inactive'  ];then
-    echo  '['date'] [Alerta] El servei mysql  no esta actiu al servidor ' hostname
+    echo  '['$(date)'] [Alerta] El servei mysql  no esta actiu al servidor ' $(hostname)
 fi
