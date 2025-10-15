1ac29e859e1e6e22f889decd8e028e47f30c9eda
0xiem <issam.moussaoui@outlook.com>
2025-10-15 06:12:24 +0200

    Se ha acabado el script

--- MonitoringServices.sh
+++ MonitoringServices.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-servicios=('apache2' 'mysql')
-FicheroLog='/var/log/monitoratge-serveis'
 
-Mensage() {
-    Correo='ewcmfg@gmail.com'
-    Subject='Urgente Mira El correo'
-}
+SERVEIS=("apache2" "mysql")
+log="/var/log/monitoratge-serveis"
+EMAIL="admin@example.com"
+HOSTNAME=
 
-for i in ${servicios[@]};do
-    if (systemctl -q is-active $i) ;then
-        echo $i active
-    else
-        echo $i No esta activo
-    fi
+while true; do
+    for i in "${SERVEIS[@]}"; do
+        if ! systemctl is-active -q $i; then
+            echo "$(date) [Alerta] El servei $i NO està actiu al servidor $(hostname)" >> $log
+            echo "El servei $i està parat. S'ha enviat una notificació a l'administrador."
+            echo "El servei $i està parat al servidor $(hostname)" | mail -s "Alerta de servei parat" $EMAIL
+            
+            for x in {1..4}; do
+                systemctl start $i
+                if systemctl is-active -q $i; then
+                    echo "$(date) [Alerta] El servei $i s'ha recuperat al servidor $(hostname)" | mail -s "Recuperació de servei" $EMAIL
+                    break
+                fi
+            done
+            
+            if ! systemctl is-active --quiet $i; then
+                echo "$(date) [Alerta Crítica] No s'ha pogut reiniciar el servei $i després de 4 intents al servidor $(hostname)" >> $log
+                echo "No s'ha pogut reiniciar el servei $i després de 4 intents. Revisar el LOG." | mail -s "Error de servei" -a $log $EMAIL
+            fi
+        fi
+    done
+    sleep 60
 done
